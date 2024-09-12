eef86031fafbc428e2c67bf6bc4f090057fa38a4
MaiaAndrey31 <maiaandrey31@outlook.com>
2024-09-12 09:53:05 -0300

    InitialCommit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,40 @@
+import { v4 } from 'uuid'
+import * as Yup from 'yup'
+import User from '../models/User'
+
+class UserController {
+  async store(request, response) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(8),
+      admin: Yup.boolean(),
+    })
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { name, email, password_hash, admin } = request.body
+
+    const userExist = await User.findOne({
+      where: { email },
+    })
+    if (userExist) {
+      return response.status(400).json({ error: 'User already exists' })
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    })
+    return response.status(201).json(user)
+  }
+}
+
+export default new UserController()
