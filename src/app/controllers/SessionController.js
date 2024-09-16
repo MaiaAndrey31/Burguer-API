4ef86b8f1de926325f8e97e2f0e627efc4de6611
MaiaAndrey31 <maiaandrey31@outlook.com>
2024-09-16 10:16:57 -0300

    featUserLogin

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,44 @@
+import * as Yup from 'yup'
+import User from '../models/User'
+
+class SessionController {
+  async store(request, response) {
+    const schema = Yup.object({
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+    })
+
+    const isValid = await schema.isValid(request.body)
+
+    const wrongUserOrPassword = () =>
+      response
+        .status(401)
+        .json({ error: 'Make sure your username or password are correct' })
+
+    if (!isValid) {
+      return wrongUserOrPassword()
+    }
+
+    const { email, password } = request.body
+
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      return wrongUserOrPassword()
+    }
+
+    const isSamePassword = await user.checkPassword(password)
+
+    if (!isSamePassword) {
+      return wrongUserOrPassword()
+    }
+    return response.status(201).json({
+      id: user.id,
+      name: user.name,
+      email,
+      admin: user.admin,
+    })
+  }
+}
+
+export default new SessionController()
