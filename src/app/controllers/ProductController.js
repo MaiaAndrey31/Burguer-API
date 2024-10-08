b3071f1f628771883c6c578e0ef3d997ba1d08b0
MaiaAndrey31 <maiaandrey31@outlook.com>
2024-10-08 10:54:57 -0300

    FeatUpdateProducts

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -6,9 +6,10 @@ import User from '../models/User'
 class ProductController {
   async store(request, response) {
     const schema = Yup.object({
-      name: Yup.string().required(),
-      price: Yup.number().required(),
-      category_id: Yup.number().required(),
+      name: Yup.string(),
+      price: Yup.number(),
+      category_id: Yup.number(),
+      offer: Yup.boolean(),
     })
 
     try {
@@ -24,16 +25,71 @@ class ProductController {
     }
 
     const { filename: path } = request.file
-    const { name, price, category_id } = request.body
+    const { name, price, category_id, offer } = request.body
 
     const product = await Product.create({
       name,
       price,
       category_id,
       path,
+      offer,
     })
 
     return response.status(201).json(product)
+  }
+
+  async update(request, response) {
+    const schema = Yup.object({
+      name: Yup.string(),
+      price: Yup.number(),
+      category_id: Yup.number(),
+      offer: Yup.boolean(),
+    })
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { id } = request.params
+
+    const findProduct = await Product.findByPk(id)
+
+    if (!findProduct) {
+      return response.status(404).json({ error: 'Product not found' })
+    }
+
+    let path
+
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    const { name, price, category_id, offer } = request.body
+
+    await Product.update(
+      {
+        name,
+        price,
+        category_id,
+        path,
+        offer,
+      },
+      {
+        where: {
+          id,
+        },
+      },
+    )
+
+    return response.status(200).json()
   }
 
   async index(request, response) {
